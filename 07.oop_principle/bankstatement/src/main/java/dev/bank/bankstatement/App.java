d9ce3ae2271224098baa1dab37783d3dbe0cc46f
KimEunhyuk <bang1676@naver.com>
2022-04-12 18:01:49 +0900

    bankstatement

---
+++ 07.oop_principle/bankstatement/src/main/java/dev/bank/bankstatement/App.java
@@ -0,0 +1,13 @@
+package dev.bank.bankstatement;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
